f3376b300a72297c661cc8ba521048dffba860f9
[vandeebullet] <[vandeebullet@icloud.com]>
2022-02-02 22:02:07 +0400

    задание 1.2 (не смог понять последнее действие выполнить,интересно будет увидеть правильный пример),если есть возможность через мой код

---
+++ task2_1.py
@@ -0,0 +1,16 @@
+num = 1
+while num <= 1000:
+    num += 1
+    if num % 2 == 0:
+        continue
+    summ = (num ** 3)
+    while summ != 0:
+        summ_1 = (summ / 7)
+       # if type(summ_1) is float:
+        # summ_1 = 0
+       # else: summ_1 * 1
+
+        break
+
+    print((summ_1))
+#summ_2 = summ_1 %10
